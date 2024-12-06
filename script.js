7aa0305344e29a417c127e847f0ff16529ce96ef
putratunggalelektronik <putratunggalelektronik@gmail.com>
2024-12-07 00:57:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-let slideIndex = 1;
+let slideIndex = 1; // Slide awal
 showSlides(slideIndex);
 
 function showSlides(n) {
   let slides = document.getElementsByClassName("slides");
   let dots = document.getElementsByClassName("dot");
 
-  
-  if (n > slides.length) slideIndex = 1;
-  if (n < 1) slideIndex = slides.length;
+  // Jika index melebihi jumlah slide, kembali ke slide pertama
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
 
+  // Jika index kurang dari 1, kembali ke slide terakhir
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
 
+  // Sembunyikan semua slide
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
 
-
+  // Nonaktifkan semua dot
   for (let i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
 
-
+  // Tampilkan slide aktif
   slides[slideIndex - 1].style.display = "block";
+
+  // Aktifkan dot yang sesuai
   dots[slideIndex - 1].className += " active";
 }
 
