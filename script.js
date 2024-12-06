b6c7234cd29d053917e998f8f68af8f46451cc33
putratunggalelektronik <putratunggalelektronik@gmail.com>
2024-12-06 15:32:50 +0700

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-lefunction changeSlide(n) {
-    let slides = document.getElementsByClassName("slides");
-    slideIndex += n;
-    if (slideIndex > slides.length) {
-      slideIndex = 1;
-    } else if (slideIndex < 1) {
-      slideIndex = slides.length;
-    }
-    showSlides();
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) slideIndex = 1;
+  if (n < 1) slideIndex = slides.length;
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function changeSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
